6128502866acfbfbfa2efd2db1c8872263119db1
Marcus-Nastasi <vinnie.nsts@gmail.com>
2024-12-26 21:49:06 -0300

    Refactoring cep controller.

--- backend/test/src/main/java/com/accenture/test/adapter/controller/cep/CepController.java
+++ backend/test/src/main/java/com/accenture/test/adapter/controller/cep/CepController.java
@@ -1,13 +1,14 @@
 package com.accenture.test.adapter.controller.cep;
 
+import com.accenture.test.adapter.output.cep.CepResponseDto;
 import com.accenture.test.application.usecase.cep.CepUseCase;
 import com.accenture.test.domain.cep.Cep;
+import com.accenture.test.infrastructure.exception.InfraException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,11 +21,25 @@ public class CepController {
     @GetMapping(value = "/{cep}")
     @Cacheable("cep")
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Buscar dados de endereço baseado no CEP", description = "Nessa rota você pode consultar dados detalhados de endereço baseado em um CEP")
-    @ApiResponse(responseCode = "200", description = "Retornando dados da API dos correios")
-    public ResponseEntity<Cep> buscar_cep(@PathVariable("cep") String cep) {
+    @Operation(summary = "Search address data based on zip code", description = "In this route you can consult detailed address data based on a zip code")
+    @ApiResponse(responseCode = "200", description = "Returning data from the postal API")
+    public CepResponseDto getByCep(@PathVariable("cep") String cep) {
         Cep cr = cepUseCase.getCep(cep);
-        if (cr == null) return ResponseEntity.badRequest().build();
-        return ResponseEntity.ok(cr);
+        if (cr == null) throw new InfraException("Could not get the address based on this cep.");
+        return new CepResponseDto(
+            cr.getCep(),
+            cr.getLogradouro(),
+            cr.getComplemento(),
+            cr.getUnidade(),
+            cr.getBairro(),
+            cr.getLocalidade(),
+            cr.getUf(),
+            cr.getEstado(),
+            cr.getRegiao(),
+            cr.getIbge(),
+            cr.getGia(),
+            cr.getDdd(),
+            cr.getSiafi()
+        );
     }
 }
--- backend/test/src/main/java/com/accenture/test/infrastructure/configuration/swagger/SwaggerConfig.java
+++ backend/test/src/main/java/com/accenture/test/infrastructure/configuration/swagger/SwaggerConfig.java
@@ -2,17 +2,13 @@ package com.accenture.test.infrastructure.configuration.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 
-@ComponentScan
-@RestController
 @Configuration
 @OpenAPIDefinition(
     info = @Info(
         title = "Company Manager",
-        description = "Uma aplicação para relacionar empresas e fornecedores",
+        description = "An application to relate companies and suppliers",
         version = "1.0.0"
     )
 )

f5e76f3c6a8ec0e4837b9a1bb0b187741e74c8e7
Marcus-Nastasi <vinnie.nsts@gmail.com>
2024-12-26 22:41:32 -0300

    Refactoring cep integration test.

--- backend/test/src/test/java/com/accenture/test/CepIntegrationTest.java
+++ backend/test/src/test/java/com/accenture/test/CepIntegrationTest.java
@@ -1,21 +1,22 @@
 package com.accenture.test;
 
 import com.accenture.test.application.usecase.cep.CepUseCase;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-public class CepConfigurationTest {
+public class CepIntegrationTest {
 
     @Autowired
     private CepUseCase cepUseCase;
 
     @Test
     void cepTest() {
-        Assertions.assertDoesNotThrow(() -> {
-            cepUseCase.getCep("04632-011");
-        });
+        assertEquals("Avenida Vicente Machado", cepUseCase.getCep("80420-010").getLogradouro());
+        assertEquals("PR", cepUseCase.getCep("80420-010").getUf());
+        assertDoesNotThrow(() -> cepUseCase.getCep("80420-010"));
     }
 }

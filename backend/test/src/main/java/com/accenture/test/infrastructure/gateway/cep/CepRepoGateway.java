f70b5ff7e7ff1e1aec39913f5d6d42d85d841e67
Marcus-Nastasi <vinnie.nsts@gmail.com>
2024-12-27 00:07:30 -0300

    Refactoring cep .

--- backend/test/src/main/java/com/accenture/test/infrastructure/gateway/cep/CepRepoGateway.java
+++ backend/test/src/main/java/com/accenture/test/infrastructure/gateway/cep/CepRepoGateway.java
@@ -3,9 +3,8 @@ package com.accenture.test.infrastructure.gateway.cep;
 import com.accenture.test.application.gateways.cep.CepGateway;
 import com.accenture.test.domain.cep.Cep;
 import com.accenture.test.infrastructure.exception.InfraException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.Objects;
 
 public class CepRepoGateway implements CepGateway {
 
@@ -17,7 +16,9 @@ public class CepRepoGateway implements CepGateway {
             cep = cep.replaceAll("\\D", "").trim();
             if (cep.length() > 8) throw new InfraException("Invalid cep");
             String url = "https://viacep.com.br/ws/" + cep + "/json/";
-            return Objects.requireNonNull(webClient.get().uri(url).retrieve().toEntity(Cep.class).block()).getBody();
+            ResponseEntity<Cep> cepResponseEntity = webClient.get().uri(url).retrieve().toEntity(Cep.class).block();
+            if (cepResponseEntity == null) throw new InfraException("Unable to get CEP, equals null");
+            return cepResponseEntity.getBody();
         } catch (RuntimeException e) {
             throw new InfraException("Error getting CEP: " + e.getMessage());
         }
